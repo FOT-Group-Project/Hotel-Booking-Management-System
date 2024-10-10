178161e1f6983f530f76ef2b5afd50c5c2144975
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-10-10 20:24:21 +0530

    Refactor frontend routing and add authentication pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">Hotel Management System</div>
+    <BrowserRouter className="text-3xl font-bold underline">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/Signup" element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
