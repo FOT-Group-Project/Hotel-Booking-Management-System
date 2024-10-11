c53aaaa9e60325d8d76d4e8d691399ef71f7be4c
Maleesha Herath <91375598+MaleeshaHe@users.noreply.github.com>
2024-10-11 15:50:29 +0530

    Refactor sign-in and sign-up pages to include form validation and error handling

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,16 +1,48 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 import logo from "../assets/logo.png";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password) {
+      return dispatch(signInFailure("Please fill all the fields"));
+    }
+    try {
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+      }
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
   };
 
   return (
@@ -30,22 +62,22 @@ export default function SignIn() {
 
         {/* right side */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4" onSubmit={null}>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <h3 className="text-2xl font-bold text-gray-700 dark:text-white ">
               Sign In
             </h3>
 
             <div>
-              <Label value="Your Email" />
+              <Label value="User Name" />
               <TextInput
-                type="email"
-                placeholder="name@company.com"
-                id="email"
+                type="text"
+                placeholder="user name"
+                id="username"
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value="Your Password" />
+              <Label value="Password" />
               <TextInput
                 type="password"
                 placeholder="***********"
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,16 +1,43 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import logo from "../assets/logo.png";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
   };
 
   return (
@@ -30,7 +57,7 @@ export default function SignUp() {
 
         {/* right side */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4" onSubmit={null}>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <h3 className="text-2xl font-bold text-gray-700 dark:text-white ">
               Sign Up
             </h3>
