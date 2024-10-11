d33f213f9e57e4c425a06b056993108f29fa24da
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-10-11 23:13:57 +0530

    Add Google Auth Functionality

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -8,6 +8,7 @@ import {
   signInFailure,
 } from "../redux/user/userSlice";
 import logo from "../assets/logo.png";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -96,6 +97,7 @@ export default function SignIn() {
                 "Sign In"
               )}
             </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont Have an account?</span>
