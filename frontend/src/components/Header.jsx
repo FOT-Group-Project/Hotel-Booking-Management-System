617abc406c924658777b4a091f825f28bbb9faae
Maleesha Herath <91375598+MaleeshaHe@users.noreply.github.com>
2024-10-12 21:02:28 +0530

    complete user sign out and user dropdown menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,36 @@
-import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon, FaSun } from 'react-icons/fa';
-import { useEffect, useState } from 'react';
+import { Button, Navbar, TextInput, Dropdown, Avatar } from "flowbite-react";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
 
 export default function Header() {
-    const path = useLocation().pathname;
-    const location = useLocation();
-    const navigate = useNavigate();
+  const path = useLocation().pathname;
+  const dispatch = useDispatch();
+
+  const { currentUser } = useSelector((state) => state.user);
+  const theme = useSelector((state) => state.theme.theme);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/auth/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <Navbar className="border-b-2">
       <Link to="/">
@@ -16,9 +38,38 @@ export default function Header() {
       </Link>
 
       <div className="flex gap-2 md:order-2">
-        <Link to="/sign-in">
-          <Button className="bg-customBlue">Sign In</Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilepicurl} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to="/dashboard?tab=profile">
+              <Dropdown.Item>
+                <span className="block text-sm">Profile</span>
+              </Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Link onClick={handleSignout}>
+              <Dropdown.Item>
+                <span className="block text-sm">Sign out</span>
+              </Dropdown.Item>
+            </Link>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button className="bg-customBlue">Sign In</Button>
+          </Link>
+        )}
+
         <Navbar.Toggle className="" />
       </div>
 
