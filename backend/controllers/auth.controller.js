f1fd0620f6a156c95f77ef99f7065f3dc9eb0196
Maleesha Herath <91375598+MaleeshaHe@users.noreply.github.com>
2024-10-12 20:54:39 +0530

    create sign out api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -69,7 +69,7 @@ function signUp(req, res) {
     });
 }
 
-const signIn = (req, res) => {
+function signIn(req, res) {
   models.User.findOne({ where: { username: req.body.username } })
     .then((user) => {
       if (user === null) {
@@ -117,54 +117,71 @@ const signIn = (req, res) => {
         error: error,
       });
     });
-};
+}
 
-const google = async (req, res, next) => {
-  const {email, name, photoURL} = req.body;
+async function google(req, res, next) {
+  const { email, name, googlePhotoUrl } = req.body;
   try {
-    const user = await User.findOne({email});
-    if(user) {
+    const user = await models.User.findOne({ email });
+    if (user) {
       const token = jwt.sign(
-        { id: user.id, role: user.role },
-        process.env.JWT_SECRET_KEY, // Token expires in 1 hour
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
       );
-      const { password: pass, ...rest } = user.dataValues;
-      return res
+      const { password, ...rest } = user._doc;
+      res
         .status(200)
         .cookie("access_token", token, {
           httpOnly: true,
-          maxAge: 3600000, // Expires in 1 hour, must be in milliseconds
         })
         .json(rest);
     } else {
-      const password = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
-      const hashedPassword = bcrypt.hashSync(password, 10);
-      const newUser = await User.create({
-        username: name,
-        email: email,
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
         password: hashedPassword,
-        profilepicurl: photoURL
+        profilePicture: googlePhotoUrl,
       });
-      await newUser.save();
+      await models.newUser.save();
       const token = jwt.sign(
-        { id: newUser.id},
-        process.env.JWT_SECRET_KEY, // Token expires in 1 hour
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
       );
-      const { password: pass, ...rest } = newUser._doc;
-      res 
+      const { password, ...rest } = newUser._doc;
+      res
         .status(200)
         .cookie("access_token", token, {
-          httpOnly: true, // Expires in 1 hour, must be in milliseconds
+          httpOnly: true,
         })
         .json(rest);
     }
   } catch (error) {
     next(error);
   }
-};
+}
+
+function signOut(req, res) {
+  try {
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json({ success: true, message: "User signed out successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error", error: error });
+  }
+}
 
 module.exports = {
   signUp: signUp,
   signIn: signIn,
-  google: google
+  google: google,
+  signOut: signOut,
 };
