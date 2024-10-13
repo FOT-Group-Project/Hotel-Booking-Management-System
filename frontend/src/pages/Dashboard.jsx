e5f33d068def71342986fa4b5c524875e272e447
Maleesha Herath <91375598+MaleeshaHe@users.noreply.github.com>
2024-10-13 19:57:21 +0530

    create slider bar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const loaction = useLocation();
+  const [tab, setTab] = useState("");
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(loaction.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [loaction.search]);
+
+  return <div>Dashboard</div>;
 }
