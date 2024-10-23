8938e8d90a7bf6ef5c345c9ab359813201c26f3c
Maleesha Herath <91375598+MaleeshaHe@users.noreply.github.com>
2024-10-23 21:24:53 +0530

    Refactor routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
-import AboutUs from './pages/AboutUs'
-import ContactUs from './pages/ContactUs'
-import Booking from './pages/Booking'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import Booking from "./pages/Booking";
 
 export default function App() {
   return (
     <BrowserRouter className="text-3xl font-bold underline">
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp/>} />
-        <Route path="/about-us" element={<AboutUs/>} />
-        <Route path="/contact-us" element={<ContactUs/>} />
-        <Route path="/booking" element={<Booking/>} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/booking" element={<Booking />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
