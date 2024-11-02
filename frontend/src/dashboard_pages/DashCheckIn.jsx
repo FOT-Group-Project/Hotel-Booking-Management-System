20dc9b0adca700d8c48e4decc91589b9417f847f
MaleeshaHe <malishaherath77@gmail.com>
2024-11-02 18:03:13 +0530

    Complete DashCheckIn

--- frontend/src/dashboard_pages/DashCheckIn.jsx
+++ frontend/src/dashboard_pages/DashCheckIn.jsx
@@ -87,7 +87,7 @@ export default function DashCheckIn() {
 
   // Pagination
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
+  const itemsPerPage = 4;
   const totalPages = Math.ceil(room.length / itemsPerPage);
 
   const onPageChange = (page) => setCurrentPage(page);
